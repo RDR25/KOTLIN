1929f03e4ebeb119c49095083673a0ee4bad5336
RajeshRDR <rajeshrdr1725@gmail.com>
2021-07-06 21:16:24 +0530

    Fragments 1.1

--- app/src/main/java/com/example/myapplication/Fragment2.kt
+++ app/src/main/java/com/example/myapplication/Fragment2.kt
@@ -1,10 +1,33 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.myapplication.databinding.Fragment2Binding
 
-class Fragment2 : Fragment(R.layout.fragment2) {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+class Fragment2 : Fragment() {
+
+    lateinit var binder : Fragment2Binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binder = Fragment2Binding.inflate(layoutInflater)
+        return binder.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binder.fInFragment2.setOnClickListener(View.OnClickListener {
+            var fragmentManager = activity?.supportFragmentManager
+            var fragmentTransaction = fragmentManager?.beginTransaction()
+            fragmentTransaction?.replace(R.id.fragmentContainerView,Fragment1())
+            fragmentTransaction?.commit()
+        })
+
     }
 }
